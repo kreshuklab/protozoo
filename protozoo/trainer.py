5ddd64459214188f4ef00f61a996b5595430e701
fynnbe <thefynnbe@gmail.com>
2019-09-02 16:31:09 +0200

    Trainer as class

--- protozoo/trainer.py
+++ protozoo/trainer.py
@@ -3,31 +3,36 @@ from ignite.engine import Events, Engine
 from protozoo.tiktorch_config_keys import ModelZooEntry
 
 
-def get_trainer(model_zoo_entry: ModelZooEntry) -> Engine:
-    def training_step(trainer: Engine, batch) -> float:
-        print("STEP")
-        ipt, tgt = batch
-        trainer.state.optimizer.zero_grad()
-        pred = trainer.state.model(ipt)
-        loss = trainer.state.loss_fn(pred, tgt)
-        loss.backward()
-        trainer.state.optimizer.step()
-        return loss.item()
+class Trainer(Engine):
+    def __init__(self, model_zoo_entry: ModelZooEntry):
+        model_config = model_zoo_entry.model_config
+        optimizer_config = model_zoo_entry.optimizer_config
+        loss_config = model_zoo_entry.loss_config
 
-    trainer = Engine(training_step)
+        if model_config.pretrained_source is not None:
+            raise NotImplementedError("model_zoo_entry.model_config.pretrained_source")
 
-    @trainer.on(Events.STARTED)
-    def training_setup(trainer: Engine):
-        trainer.state.model = model_zoo_entry.model_config.model_class(**model_zoo_entry.model_config.model_kwargs)
-        assert model_zoo_entry.model_config.pretrained_source is None
-        trainer.state.optimizer = model_zoo_entry.optimizer_config.optimizer_class(
-            trainer.state.model.parameters(), **model_zoo_entry.optimizer_config.optimizer_kwargs
-        )
-        trainer.state.loss_fn = model_zoo_entry.loss_config.loss_class(
-            trainer.state.model.parameters(), **model_zoo_entry.loss_config.loss_kwargs
-        )
+        def training_step(trainer: Engine, batch) -> float:
+            print("STEP")
+            ipt, tgt = batch
+            trainer.state.optimizer.zero_grad()
+            pred = trainer.state.model(ipt)
+            loss = trainer.state.loss_fn(pred, tgt)
+            loss.backward()
+            trainer.state.optimizer.step()
+            return loss.item()
 
-    for callback in model_zoo_entry.trainer_callbacks:
-        trainer.add_event_handler(callback.event, callback.function)
+        super().__init__(training_step)
 
-    return trainer
+        @self.on(Events.STARTED)
+        def setup(trainer: Engine):
+            model = model_config.model_class(**model_config.model_kwargs)
+            trainer.state.optimizer = optimizer_config.optimizer_class(
+                model.parameters(), **optimizer_config.optimizer_kwargs
+            )
+            trainer.state.loss_fn = loss_config.loss_class(model.parameters(), **loss_config.loss_kwargs)
+            model.train()
+            trainer.state.model = model
+
+        for callback in model_zoo_entry.trainer_callbacks:
+            self.add_event_handler(callback.event, callback.function)
