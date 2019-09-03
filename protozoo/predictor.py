85a6c813b0ce3fb662353a48574f53854a6e4c3e
fynnbe <thefynnbe@gmail.com>
2019-09-03 14:31:32 +0200

    setup and step function as staticmethods

--- protozoo/predictor.py
+++ protozoo/predictor.py
@@ -11,24 +11,26 @@ class Predictor(Engine):
         if model_zoo_entry.model_config.pretrained_source is not None:
             raise NotImplementedError("model_zoo_entry.model_config.pretrained_source")
 
-        model_config = model_zoo_entry.model_config
+        self.model_config = model_zoo_entry.model_config
 
-        def predict_batch(predictor: Engine, batch) -> torch.Tensor:
-            print("predict")
-            return predictor.state.model(batch)
+        super().__init__(self.predict_batch)
 
-        super().__init__(predict_batch)
-
-        self.given_model = model
-        if model is not None and not isinstance(model, model_config.model_class):
+        if model is not None and not isinstance(model, self.model_config.model_class):
             raise ValueError(
-                f"model {model} is not of type {model_config.model_class} as specified in model_config.model_class"
+                f"model {model} is not of type {self.model_config.model_class} as specified in model_config.model_class"
             )
 
-        @self.on(Events.STARTED)
-        def setup(predictor: Predictor):
-            predictor.state.model = predictor.given_model or model_config.model_class(**model_config.model_kwargs)
-            predictor.state.model.eval()
+        self.model = model or self.model_config.model_class(**self.model_config.model_kwargs)
 
+        self.add_event_handler(Events.STARTED, self.setup)
         for callback in model_zoo_entry.predictor_callbacks:
             self.add_event_handler(callback.event, callback.function)
+
+    @staticmethod
+    def setup(predictor: "Predictor"):
+        predictor.model.eval()
+
+    @staticmethod
+    def predict_batch(predictor: "Predictor", batch) -> torch.Tensor:
+        print("predict")
+        return predictor.model(batch)
--- protozoo/trainer.py
+++ protozoo/trainer.py
@@ -5,34 +5,37 @@ from protozoo.tiktorch_config_keys import ModelZooEntry
 
 class Trainer(Engine):
     def __init__(self, model_zoo_entry: ModelZooEntry):
-        model_config = model_zoo_entry.model_config
-        optimizer_config = model_zoo_entry.optimizer_config
-        loss_config = model_zoo_entry.loss_config
+        self.model_config = model_zoo_entry.model_config
+        self.optimizer_config = model_zoo_entry.optimizer_config
+        self.loss_config = model_zoo_entry.loss_config
 
-        if model_config.pretrained_source is not None:
+        if self.model_config.pretrained_source is not None:
             raise NotImplementedError("model_zoo_entry.model_config.pretrained_source")
 
-        def training_step(trainer: Engine, batch) -> float:
-            print("STEP")
-            ipt, tgt = batch
-            trainer.state.optimizer.zero_grad()
-            pred = trainer.state.model(ipt)
-            loss = trainer.state.loss_fn(pred, tgt)
-            loss.backward()
-            trainer.state.optimizer.step()
-            return loss.item()
+        super().__init__(self.training_step)
 
-        super().__init__(training_step)
-
-        @self.on(Events.STARTED)
-        def setup(trainer: Engine):
-            model = model_config.model_class(**model_config.model_kwargs)
-            trainer.state.optimizer = optimizer_config.optimizer_class(
-                model.parameters(), **optimizer_config.optimizer_kwargs
-            )
-            trainer.state.loss_fn = loss_config.loss_class(model.parameters(), **loss_config.loss_kwargs)
-            model.train()
-            trainer.state.model = model
-
+        self.add_event_handler(Events.STARTED, self.setup)
         for callback in model_zoo_entry.trainer_callbacks:
             self.add_event_handler(callback.event, callback.function)
+
+    @staticmethod
+    def training_step(trainer: "Trainer", batch) -> float:
+        print("STEP")
+        ipt, tgt = batch
+        trainer.state.optimizer.zero_grad()
+        pred = trainer.model(ipt)
+        loss = trainer.state.loss_fn(pred, tgt)
+        loss.backward()
+        trainer.state.optimizer.step()
+        return loss.item()
+
+    @staticmethod
+    def setup(trainer: "Trainer"):
+        trainer.model = trainer.model_config.model_class(**trainer.model_config.model_kwargs)
+        trainer.state.optimizer = trainer.optimizer_config.optimizer_class(
+            trainer.model.parameters(), **trainer.optimizer_config.optimizer_kwargs
+        )
+        trainer.state.loss_fn = trainer.loss_config.loss_class(
+            trainer.model.parameters(), **trainer.loss_config.loss_kwargs
+        )
+        trainer.model.train()
